1f31ba4697bc2d4d5d14524e5445b6800d7adbf9
Hendika N <hendika.new@gmail.com>
2023-12-15 00:26:13 +0700

    Fix bug in permission: checking user property for non-logged in user

--- utils/views.py
+++ utils/views.py
@@ -7,19 +7,23 @@ from user.models import UserTypeChoices
 # Company owner permissions
 class IsOwnerUser(IsAuthenticated):
     def has_permission(self, request, view):
-        is_authenticated = super().has_permission(request, view)
+        # will not check other conditions if user is not authenticated
+        if not super().has_permission(request, view):
+            return False
+
         is_staff = request.user.is_staff
         is_owner_user = request.user.user_type == UserTypeChoices.OWNER
 
-        return is_authenticated and is_staff and is_owner_user
+        return is_staff and is_owner_user
 
 
 class IsCompanyOwner(IsOwnerUser):
     def has_permission(self, request, view):
-        is_owner_user = super().has_permission(request, view)
-        has_company = request.user.company is not None
+        # will not check the other condition if user_type is not OWNER
+        if not super().has_permission(request, view):
+            return False
 
-        return is_owner_user and has_company
+        return request.user.company is not None
 
 
 # Views
