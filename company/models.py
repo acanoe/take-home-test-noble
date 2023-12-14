6e638a2bbd18938680f9c908dc6231528d941b7e
Hendika N <hendika.new@gmail.com>
2023-12-15 00:34:15 +0700

    Change verbose name for Company model

--- company/models.py
+++ company/models.py
@@ -12,3 +12,7 @@ class Company(UserTimestampedAbstract):
     
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Company'
+        verbose_name_plural = 'Companies'
